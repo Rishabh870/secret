4a0035ffb7c25666487b04b6dd33da71320d2d69
Rishabh870 <67893503+Rishabh870@users.noreply.github.com>
2021-08-20 16:50:39 +0530

    lvl 2 Auth

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -20,6 +20,7 @@ const userSchema =  new mongoose.Schema({
   password: String
 })
 
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User= new mongoose.model("User",userSchema)
 
